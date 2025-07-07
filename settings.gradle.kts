9f5a5852cecfc91ad6b3aac3334205a2c4c4d6c2
minhnd299 <minhndhe172328@fpt.edu.vn>
2025-07-07 11:41:12 +0700

    Revert "test firebase 1"
    This reverts commit fe746ca5a53267d7602f986f8df2a790c0c1e2be.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,6 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        maven {
-            url = uri("https://jitpack.io")
-        }
     }
 }
 dependencyResolutionManagement {
@@ -19,9 +16,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
     }
 }
 
