e8a6e5c8fa4de2da3a3031ad59d3aa850b2ef899
minhnd299 <minhndhe172328@fpt.edu.vn>
2025-07-09 13:58:38 +0700

    upload add item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,14 +29,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
